644c3b2d2ea86ce0234b43c85041d4dc68aa8a55
name = ER!CK <38144008+ERICK-ZABALA@users.noreply.github.com>
2023-09-01 06:52:12 +0000

    updated: paramiko router

--- network/paramiko/ssh_paramiko_01.py
+++ network/paramiko/ssh_paramiko_01.py
@@ -1,4 +1,8 @@
-#! /workspaces/DEVNET/network/iosv/bin/python
+#!/workspaces/DEVNET/network/iosv/bin/python
+
+import time 
+import json 
+import paramiko
 
 """
 First Paramiko Program
@@ -7,8 +11,6 @@ Environment: EVE-NG
 Router: CSR1000V
 Library: paramiko
 """
-import paramiko
-import time, json
 
 with open('devices.json', 'r') as f:
     devices = json.load(f)
@@ -20,20 +22,33 @@ with open('commands.txt', 'r') as f:
 
 print(commands)
 
+# Define una lista de algoritmos que deseas deshabilitar
+#disabled_algorithms = {
+#    'kex': ['ecdh-sha2-nistp256', 'ecdh-sha2-nistp384'],
+#    'server_host_key': ['ssh-rsa'],
+#    'cipher': ['aes128-cbc']
+#}
+# disabled_algorithms=disabled_algorithms
+
 # copy the public key in the folder .ssh in your remote SERVER or ROUTER
+
 key = paramiko.RSAKey.from_private_key_file('/home/codespace/.ssh/id_rsa')
 
 for device in devices.keys():
     connection = paramiko.SSHClient()
     connection.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    paramiko.common.logging.basicConfig(level=paramiko.common.DEBUG)
+    print(devices[device]['ip'])
     
-    connection.connect(devices[device]['ip'], 
+    connection.connect(devices[device]['ip'],
+                       port= 922, 
                        username=devices[device]['username'], 
-                       port=devices[device]['ip'],  
                        pkey=key,
                        look_for_keys=False,
-                       allow_agent=False)
-
+                       allow_agent=False,
+                       disabled_algorithms=dict(pubkeys=["rsa-sha2-512", "rsa-sha2-256"])
+                       )
+    
     new_connection = connection.invoke_shell()
     output = new_connection.recv(5000)
     print(output)
@@ -42,6 +57,7 @@ for device in devices.keys():
     time.sleep(3)
     output = new_connection.recv(5000)
     print(output)
+
 
     new_connection.close()
     
